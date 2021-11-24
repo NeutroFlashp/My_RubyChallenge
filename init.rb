b4e0121f772b1833287400b635aac0cc42af2fa0
Miguel Lorenzo <neutroflashp@hotmail.com>
2021-11-24 18:06:51 -0500

    Añadiendo color a la consola Se procedio a dar color a la consola para una mejor vista estetica se instalo la gema colorize para esto

--- init.rb
+++ init.rb
@@ -1,29 +1,37 @@
 # frozen_string_literal: false
 
 require_relative './products'
+require 'colorize' #gema para colores
 
 # Write the program here
 
 def menu
-  puts 'MENU'
-  puts '1. Show stocks'
-  puts '2. Add new products'
-  puts '3. Retire products'
-  puts '4. Show movements of product'
-  puts '5. Exit'
-  print 'Select option: '
+  puts 'MENU'.red
+  print '1.'.blue
+  puts 'Show stocks'.yellow
+  print '2.'.blue
+  puts 'Add new products'.yellow
+  print '3.'.blue
+  puts 'Retire products'.yellow
+  print '4.'.blue
+  puts 'Show movements of product'.yellow
+  print '5.'.blue
+  puts 'Exit'.yellow
+  print 'Select option: '.blue
   gets.to_i
 end
 
 def stocks
   PRODUCTS.each do |hash_products|
-    puts "______________________________________________"
+    puts "________________________________________".green
     hash_products.each do |symbol, value|
-      print "| #{symbol.upcase} = #{value} |"
+      print "|".green
+      print "#{symbol.upcase} = #{value}"
+      print "|".green
     end
     puts " "
   end
-  print "______________________________________________"
+  print "________________________________________".green
 end
 
 def add
@@ -44,7 +52,7 @@ loop do
   case menu
   when 1
     stocks
-    sleep 10
+    sleep 10 #tiempo(en seg ) de espera para que el usuario vea la tabla
   when 2
     add
 
@@ -55,11 +63,13 @@ loop do
     movements
 
   when 5
-    print 'Closing the program!'
+    print 'Closing the program!'.red
     exit(0)
 
   else
-    puts 'Select a correct option'
+    print 'ERROR! '.yellow
+    puts 'Select a correct option'.red
+    sleep 2 #tiempo (en seg) de espera para que el usuario pueda leer el mensaje
   end
   puts "\e[H\e[2J"
 end
