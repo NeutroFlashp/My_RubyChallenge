2b47d900c9bf4a783deefb115df1319b97342b7e
Miguel Lorenzo <neutroflashp@hotmail.com>
2021-11-26 18:00:20 -0500

    Avance case 3 e implementacion de clases Se procedio a avanzar el case #3 y se añadio clases y atributos a los metodos

--- init.rb
+++ init.rb
@@ -4,94 +4,102 @@ require_relative './products'
 require 'colorize' #gema para colores
 
 # Write the program here
-
-def menu
-  puts 'MENU'.red
-  print '1.'.blue
-  puts 'Show stocks'.yellow
-  print '2.'.blue
-  puts 'Add new products'.yellow
-  print '3.'.blue
-  puts 'Retire products'.yellow
-  print '4.'.blue
-  puts 'Show movements of product'.yellow
-  print '5.'.blue
-  puts 'Exit'.yellow
-  print 'Select option: '.blue
-  gets.to_i
+class Program #clase #1
+  def self.list #atributo 1
+    puts 'MENU'.red
+    print '1.'.blue
+    puts 'Show stocks'.yellow
+    print '2.'.blue
+    puts 'Add new products'.yellow
+    print '3.'.blue
+    puts 'Retire products'.yellow
+    print '4.'.blue
+    puts 'Show movements of product'.yellow
+    print '5.'.blue
+    puts 'Exit'.yellow
+    print 'Select option: '.blue
+    @value1 = gets.to_i #variable_de_instancia
+  end
 end
 
-def stocks
-  PRODUCTS.each do |hash_products|
-    puts "________________________________________".green
-    hash_products.each do |symbol, value|
-      print "|".green
-      print "#{symbol.upcase} = #{value}"
-      print "|".green
+class Board #clase 2
+  def self.show #atributo 2
+    $list = PRODUCTS.map(&:clone) #variable_global #clonacion de products.rb
+    PRODUCTS.each do |hash_products|
+      puts "________________________________________".green
+      hash_products.each do |symbol, value|
+        print "|".green
+        print "#{symbol} = #{value}"
+        print "|".green
+      end
+      puts " "
     end
-    puts " "
+    print "________________________________________".green
   end
-  print "________________________________________".green
 end
 
-def add
-  new_product = Hash.new
+class Add # clase 3
+  def self.new_products #atributo 3
+    new_product = Hash.new
+    print 'Inserte el nuevo tipo: '.red
+    new_type = gets.chomp.upcase
+    new_product["type"] = new_type
 
-  print('Inserte el nuevo tipo: ')
-  new_type = gets.chomp
-  new_product["type"] = new_type
+    print 'Inserte la cantidad: '.red
+    new_quantity = gets.to_i
+    new_product["quantity"] = new_quantity
 
-  print('Inserte la cantidad: ')
-  new_quantity = gets.to_i
-  new_product["quantity"] = new_quantity
+    print 'Inserte el costo unitario: '.red
+    new_unit_cost = gets.to_i
+    new_product["unit_cost"] = new_unit_cost
 
-  print('Inserte el costo unitario: ')
-  new_unit_cost = gets.to_i
-  new_product["unit_cost"] = new_unit_cost
-
-  PRODUCTS.push(new_product)
-  puts 'El nuevo producto fue añadido'
+    PRODUCTS.push(new_product)
+    puts 'El nuevo producto fue añadido!'.yellow
+    sleep 4
+  end
 end
 
-def retire
-  #print 'selecciona un tipo: '
-  #selected_type = gets.chomp
-
-  #if PRODUCTS["type"] == selected_type
-
-  #end
+class Retire #clase 4
+  def self.products # atributo 4
+    print 'Selecciona un tipo: '.red
+    selected_type = gets.chomp.upcase
+    print 'Selecciona la cantidad: '.red
+    selected_quantity = gets.to_i
+    if selected_type == list[0][:type]
+      if selected_quantity < list[0][:quantity]
+        list[0][:quantity] - selected_quantity
+        puts "El producto se retiro con exito!".blue
+      else
+        print "ERROR! ".yellow
+        puts "La solicitud excede el stock actual".red
+      end
+    end
+  end
 end
 
-def movements
-  puts 'movements'
-end
 
-loop do
-
-  case menu
-
-  when 1
-    stocks
-    sleep 10 #tiempo(en seg )
-
-  when 2
-    add
-
-  when 3
-    retire
-
-  when 4
-    movements
-
-  when 5
-    print 'Closing the program!'.red
-    exit(0)
-
-  else
-    print 'ERROR! '.yellow
-    puts 'Select a correct option'.red
-    sleep 3 #tiempo (en seg)
+  def movements
+    puts 'movements'
   end
 
-  puts "\e[H\e[2J"
-end
+  loop do
+    case Program.list #llamando clase y atributo 1
+    when 1
+      Board.show #llamando clase y atributo 2
+      sleep 10 #tiempo(en seg )
+    when 2
+      Add.new_products #llamando clase y atributo 3
+    when 3
+      Retire.products #llamando clase y atributo 4
+    when 4
+      movements
+    when 5
+      print 'Closing the program!'.red
+      exit(0)
+    else
+      print 'ERROR! '.yellow
+      puts 'Select a correct option'.red
+      sleep 3 #tiempo (en seg)
+    end
+    puts "\e[H\e[2J"
+  end
